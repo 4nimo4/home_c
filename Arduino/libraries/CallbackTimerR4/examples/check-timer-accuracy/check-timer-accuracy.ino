9ad48570608b9dde6b5afacb7c78858e41d7810a
Mikhail Zhdanov <4nimo4@gmail.com>
2026-04-08 13:09:05 +0800

    ARDUINO HW7

---
+++ Arduino/libraries/CallbackTimerR4/examples/check-timer-accuracy/check-timer-accuracy.ino
@@ -0,0 +1,70 @@
+/*
+  Example of CallbackTimerR4
+
+  Copyright (c) 2024 embedded-kiddie All Rights Reserved.
+
+  This software is released under the MIT License.
+  http://opensource.org/licenses/mit-license.php
+*/
+#include "Arduino.h"
+#include "CBTimer.h"
+
+#if 1
+#define TIME_FUNCTION millis  // Check in milliseconds
+#define TIME_SCALE    1
+#else
+#define TIME_FUNCTION micros  // Check in microseconds
+#define TIME_SCALE    1000
+#endif
+
+#define TIME_PERIOD_MS    10 // 10, 50, 100, 500, 1000, 1500, 2000, 3000, ...[msec]
+#define TIME_MEASUREMENT  (60000 * TIME_SCALE)  // measurement for 1 minute
+
+static CBTimer timer;
+static volatile uint32_t interrupts = 0;
+static volatile uint32_t start_time;
+static volatile uint32_t begin, end, lap;
+static volatile uint32_t elapsed, period;
+
+void user_callback(void) {
+  end = TIME_FUNCTION();
+
+  elapsed = end - begin;
+  period  = end - lap;
+  
+  lap = end;
+  interrupts++;
+
+  // Blink LED
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Initializing a periodic timer
+  timer.begin(TIMER_MODE_PERIODIC, TIME_PERIOD_MS, user_callback, false);
+
+  // Start the timer
+  start_time = begin = lap = TIME_FUNCTION();
+  timer.start();
+}
+
+void loop() {
+  // Capture interrupt 
+  if (end) {
+    end = 0;
+    Serial.println(String(elapsed) + " (" + String(period) + ")");
+  }
+
+  // Stop measurement
+  if (TIME_FUNCTION() - start_time > TIME_MEASUREMENT) {
+    timer.stop();
+    Serial.println("Number of interrupts = " + String(interrupts));
+    while (1);
+  }
+}
---
+++ Arduino/libraries/CallbackTimerR4/examples/dimming_and_blinking/dimming_and_blinking.ino
@@ -0,0 +1,44 @@
+/*
+  Example of CallbackTimerR4
+
+  Copyright (c) 2024 embedded-kiddie All Rights Reserved.
+
+  This software is released under the MIT License.
+  http://opensource.org/licenses/mit-license.php
+*/
+#include "Arduino.h"
+#include "CBTimer.h"
+
+void callback_func(void) {
+  static int n = 0;
+
+  if (n++ % 2) {
+    digitalWrite(LED_TX, HIGH);
+    digitalWrite(LED_RX,  LOW);
+  } else {
+    digitalWrite(LED_TX,  LOW);
+    digitalWrite(LED_RX, HIGH);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_TX, OUTPUT);
+  pinMode(LED_RX, OUTPUT);
+
+  static CBTimer timer;
+  timer.begin(100, callback_func);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 256; i++) {
+    analogWrite(LED_BUILTIN, i);
+    delay(2);
+  }
+
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(LED_BUILTIN, i);
+    delay(2);
+  }
+}
